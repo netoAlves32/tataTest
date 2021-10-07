194ffae7fc5ff356e268e9fba6af91c66cd9c462
netoAlves32 <harvin39@gmail.com>
2021-10-07 01:49:12 -0500

    task and userinterfaces

---
+++ src/test/java/co/com/tata/stepdefinitions/addProductStepDefinition.java
@@ -0,0 +1,35 @@
+package co.com.tata.stepdefinitions;
+
+import co.com.tata.tasks.Complete;
+import co.com.tata.tasks.DiligenceData;
+import co.com.tata.tasks.OpenUp;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
+
+public class addProductStepDefinition {
+
+    @Given("^than Luz wants to add product/s of the web page$")
+    public void than_Luz_wants_to_add_product_s_of_the_web_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Luz login on the demoblaze web page$")
+    public void luz_login_on_the_demoblaze_web_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Luz finally join on laptops category$")
+    public void luz_finally_join_on_laptops_category() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Luz is finally selecting MacBook Pro for add to Cart$")
+    public void luz_is_finally_selecting_MacBook_Pro_for_add_to_Cart() {
+
+
